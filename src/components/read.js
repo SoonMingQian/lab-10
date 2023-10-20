6543570c004def261eeb626ff0ad2e48cc5b83e5
G00438454 <g00438454@atu.ie>
2023-10-20 09:34:43 +0000

    lab 4

--- src/components/read.js
+++ src/components/read.js
@@ -1,27 +1,27 @@
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import Books from "./books";
 import axios from "axios";
 
 function Read() {
 
-const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
 
 
-useEffect(
-    ()=>{
-        axios.get("https://jsonblob.com/api/jsonblob/1161593332966481920")
-        .then(
-            (response)=>{
-                setData(response.data.books);
-            }
-        )
-        .catch(
-            (error)=>{
-                console.log(error);
-            }
-        )
-    },[]
-);
+    useEffect(
+        () => {
+            axios.get("https://jsonblob.com/api/jsonblob/1161593332966481920")
+                .then(
+                    (response) => {
+                        setData(response.data.books);
+                    }
+                )
+                .catch(
+                    (error) => {
+                        console.log(error);
+                    }
+                )
+        }, []
+    );
 
     return (
         <div>
